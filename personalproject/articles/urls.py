bdaa28097788f2d22e40b5d5e63629c2292df984
li-olin <pilotthunder83@gmail.com>
2026-02-26 13:50:04 -0600

    n

--- personalproject/articles/urls.py
+++ personalproject/articles/urls.py
@@ -3,9 +3,10 @@ from . import views
 
 urlpatterns = [
     path('articles/', views.articles, name='articles'),
-    path(
-        '<slug:page>/',
-         views.Page,
-         name='page'
-    ),
+    # path(
+    #     '<slug:page>/',
+    #      views.Page,
+    #      name='page'
+    # ),
+    path('login/', views.user_login, name='login'),
 ]
--- personalproject/articles/views.py
+++ personalproject/articles/views.py
@@ -3,9 +3,33 @@ from django.http import Http404
 from django.http import HttpResponse
 from django.template import loader
 
-
 # Create your views here.
 from .models import Page
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(
+                request,
+                username=cd['username'],
+                password=cd['password']
+            )
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Authenticated successfully')
+                else:
+                    return HttpResponse('Disabled account')
+            else:
+                return HttpResponse('Invalid login')
+    else:
+        form = LoginForm()
+    return render(request, 'pages/login.html', {'form': form})
+
 
 def articles(request):
     template = loader.get_template('base.html')
